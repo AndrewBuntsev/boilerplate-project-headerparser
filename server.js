df2d646758cfb42543024cd0771800a7856e8ed8
ABuntsev <andrew.buntsev@gmail.com>
2019-04-09 06:59:58 +1000

    fixed port number

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get("/api/whoami", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(/*process.env.PORT*/12345, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
